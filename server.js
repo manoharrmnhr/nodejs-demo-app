c7e390e875be215a483d4d78e3e5bf8b16871c4f
manohar r <manoharrmnhr@gmail.com>
2025-10-20 17:06:12 +0530

    Add CI/CD GitHub Actions workflow

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('🚀 Hello from the automated CI/CD Node.js App!');
+});
+
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`);
+});
